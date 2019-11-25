ac989fa8d1e2de365f28d4e8cca043dbd241d20f
dark <dark@dark.lan>
2019-11-25 08:50:13 +0800

    添加部署脚本

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,14 @@
+// vue.config.js
+
 module.exports = {
-    chainWebpack: config => {
-        config.module.rule('tea')
-            .test(/\.tea$/)
-            .use('tea-loader')
-            .loader('tea-loader')
-            .end()
-    }
-}
+  assetsDir: "./",
+  publicPath: "/lang-online/",
+  chainWebpack: config => {
+    config.module
+      .rule("tea")
+      .test(/\.tea$/)
+      .use("tea-loader")
+      .loader("tea-loader")
+      .end();
+  }
+};
